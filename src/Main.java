0ec1f57d19a4d7ed7f13be7bb5bebcfb9d0952ce
TomasRosa <tomilautialdosivi@gmail.com>
2023-04-04 12:10:55 -0300

    cargar arreglo video club con peliculas

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("CARGAR ARRAYLIST CON PELICULAS.");
+        Pelicula peli = new Pelicula("John Wick",101,"R","USA","Un exasesino busca venganza contra un grupo de gángsters que mataron a su perro.","Accion",2,2014);
+        Pelicula peli2 = new Pelicula ("Mad Max: Fury Road",120,"R","AUS/USA","Un guerrero postapocalíptico ayuda a un grupo de mujeres a escapar de un líder opresor.","Accion",3,2015);
+
+        Pelicula peli3 = new Pelicula("Indiana Jones and the Raiders of the Lost Ark",115,"PG","USA","El arqueólogo Indiana Jones busca el Arca de la Alianza antes de que caiga en manos nazis.","Aventura",1,1981);
+        Pelicula peli4 = new Pelicula("The Lord of the Rings: The Fellowship of the Ring",178,"PG","NZ/USA","Un grupo de aventureros emprende una peligrosa misión para destruir un anillo que amenaza con destruir el mundo.","Aventura",7,2001);
+
+        Pelicula peli5 = new Pelicula("The Hangover",100,"R","USA","Un grupo de amigos despiertan después de una noche de desenfreno en Las Vegas sin recordar nada de lo sucedido.","Comedia",3,2009);
+        Pelicula peli6 = new Pelicula("Bridesmaids",125, "R", "USA","Una dama de honor intenta hacer frente a la caótica boda de su mejor amiga.","Comedia",4,2011);
+
+        Pelicula peli7 = new Pelicula("The Shawshank Redemption",142,"R","USA","Un banquero es encarcelado por asesinato y lucha por su supervivencia en la prisión de Shawshank.","Drama",2,1994);
+        Pelicula peli8 = new Pelicula("The Godfather",175,"R","USA","La historia épica de la familia Corleone y su lucha por el poder en el mundo del crimen organizado","Drama",3,1972);
+
+        Pelicula peli9 = new Pelicula("The Exorcist",122,"R","USA","Una madre lucha por salvar a su hija poseída por una entidad demoníaca.","Horror",3,1973);
+        Pelicula peli10= new Pelicula("The Conjuring",112,"R","USA","Una pareja de investigadores paranormales ayuda a una familia a lidiar con una presencia maligna en su casa.","Horror",2,2013);
+
+        Pelicula peli11 = new Pelicula("Blackfish",83,"PG-13","USA","El documental sigue la vida de una orca en cautiverio y su impacto en la industria del entretenimiento marino.","Documental",1,2013);
+        Pelicula peli12 = new Pelicula("The Social Dilemma",94,"PG-13","USA","El documental explora cómo las redes sociales están diseñadas para manipular la conducta humana y su impacto en la sociedad.","Documental",5,2020);
+
+
+
+    }
+}
